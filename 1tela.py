2dcaa50c3bb555f94d27edb32da53382e9c71e6d
marcos <victor.kernel14@gmail.com>
2019-08-22 22:56:00 -0300

    Commit para adição dos arquivos de tela e plot de gráfico simples

---
+++ 1tela.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import matplotlib.pyplot
+
+class Aplicacao:
+    def __init__(self, master=None):
+        self.title = Label(root, text="CTGvirtual")
+        self.title["font"] = ("Arial", "100", "bold")
+        self.title["bg"] = ("#5271FF")
+        self.title.pack(side="top", expand=1)
+        self.initButton = Button(root, text="INICIAR")
+        self.initButton["font"] = ("Arial", "30")
+        self.initButton["width"] = 20
+        self.initButton["bg"] = ("white")
+        self.initButton["command"] = self.plot
+        self.initButton.pack()
+        self.tutorialButton = Button(root, text="TUTORIAL")
+        self.tutorialButton["font"] = ("Arial", "30")
+        self.tutorialButton["width"] = 20
+        self.tutorialButton["bg"] = ("white")
+        self.tutorialButton.pack(side="bottom", expand=1)
+
+    def plot(self):
+        meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho']
+        valores = [105235, 107697, 110256, 109236, 108859, 109986]
+
+        matplotlib.pyplot.plot(meses,valores)
+        matplotlib.pyplot.show()
+
+
+root = Tk()
+root.title("CTGvirtual")
+root.geometry("1280x720")
+root["bg"] = ("#5271FF")
+Aplicacao(root)
+root.mainloop()
